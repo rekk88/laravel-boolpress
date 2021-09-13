19057717bbbc06349f8fb150937b01ca029228da
maicol <maicol.matteoli@gmail.com>
2021-09-13 18:07:08 +0200

    edit : cambiata la rotta in modo da far riferimento al controller fuori da admin

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-});
+Route::get('/', 'HomeController@index')->name('homepage');
 
 Auth::routes();
 
