bc7c10123238deb2123417cad4ad0321c172b6b3
maicol <maicol.matteoli@gmail.com>
2021-09-13 18:43:26 +0200

    fix : spazio mancante nel title (con uno slug sbagliato come conseguenza)

--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -16,7 +16,7 @@ class PostsSeeder extends Seeder
         for($i=0 ; $i < 10 ; $i++){
           $newPost = new Post(); //creo una nuova istanza di Post
 
-          $newPost->title = "post numero" . ($i + 1);
+          $newPost->title = "post numero " . ($i + 1);
           $newPost->slug = Str::slug($newPost->title ,'-');
           $newPost->content = 'Lorem ipsum dolor sit amet consectetur adipisicing 
                               elit. Eveniet, consequatur commodi? 
