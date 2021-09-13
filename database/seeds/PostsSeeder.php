a7113c3730427f9eb1edcf4244692bfa4c495bdd
maicol <maicol.matteoli@gmail.com>
2021-09-13 18:32:30 +0200

    add : aggiunto Illuminate\Support\Str

--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str; //per far funzionare la funzione che genera lo slug
+use App\Post; //includo il model
 
 class PostsSeeder extends Seeder
 {
@@ -11,6 +13,10 @@ class PostsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for($i=0 ; $i < 10 ; $i++){
+          $newPost = new Post(); //creo una nuova istanza di Post
+        }
+
+        $newPost->title = "post numero" . ($i + 1);
     }
 }
